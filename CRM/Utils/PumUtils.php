65a6ecd9363d493c4192ee0c6bbcdc11e8459ed6
Erik Hommel <hommel@ee-atwork.nl>
2013-11-25 18:11:11 +0100

    tested version, finished but for core issue with disabling contact type

--- CRM/Utils/PumUtils.php
+++ CRM/Utils/PumUtils.php
@@ -23,9 +23,46 @@ class CRM_Utils_PumUtils {
      * @author Erik Hommel (erik.hommel@civicoop.org)
      * @date 24 Nov 2013
      * @param $params containing either label, name or column_name
-     * @return $customFieldId
+     * @return $result array with id, is_error and possibly error_message
      */
     static function retrieveCustomFieldId($params) {
-        
+        /*
+         * requires label, name or column_name
+         */
+        $customFieldId = 0;
+        $result = array();
+        if (empty($params)) {
+            $result['is_error'] = 1;
+            $result['error_message'] = "Empty parameters passed. Label, name or column_name is required.";
+            return $result;
+        }
+        if (!is_array($params)) {
+            return $customFieldId;
+        }
+        if (!isset($params['label']) && !isset($params['name']) && !isset($params['column_name'])) {
+            return $customFieldId;
+        }
+        if (isset($params['label'])) {
+            $customFieldParams['label'] = (string) $params['label'];
+        }
+        if (isset($params['name'])) {
+            $customFieldParams['name'] = (string) $params['name'];
+        }
+        try {
+            $apiResult = civicrm_api3('CustomField', 'Getsingle', $params);
+        }
+        catch (CiviCRM_API3_Exception $e) {
+            $result['is_error'] = 1;
+            $result['error_message'] = "Error with OptionGroup Getsingle API : ".$e->getMessage();
+            return $result;
+        }
+        if (isset($apiResult['id'])) {
+            $result['is_error'] = 0;
+            $result['id'] = $apiResult['id'];
+        } else {
+            $result['is_error'] = 1;
+            $result['error_message'] = "No id found in result of OptionGroup Getsingle API";
+        }
+        return $result;
     }
 }
