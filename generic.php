e0319202c9e191c0ece3454446773a95270d100b
Erik Hommel <hommel@ee-atwork.nl>
2013-12-02 17:57:19 +0100

    tested and approved locally

--- generic.php
+++ generic.php
@@ -1,18 +1,156 @@
 <?php
 require_once 'generic.civix.php';
 /**
+ * Implementation of hook_civicrm_install
+ * 
+ * @author Erik Hommel (erik.hommel@civicoop.org - http://www.civicoop.org)
+ * @date 2 Dec 2013
+ * 
+ * Generate contact subtype(s), activity type(s) for PUM
+ */
+function generic_civicrm_install() {
+    $requiredContactTypes = array(
+        array(
+            'name'      =>  "Expert",
+            'parent'   =>  1
+        )
+    );
+    $requiredActivityTypes = array("First Interview Expert");
+    /*
+     * contactTypes
+     */
+    $createdContactTypes = array();
+    foreach ($requiredContactTypes as $contactType) {
+        /*
+         * only if Contact Type does not exist yet
+         */
+        try {
+            civicrm_api3('ContactType', 'Getsingle', array('title' => $contactType['name']));
+        } catch (CiviCRM_API3_Exception $e) {
+            $contactTypeParams = array(
+                'label'         =>  $contactType['name'],
+                'name'          =>  $contactType['name'],
+                'parent_id'     =>  $contactType['parent'],
+                'is_active'     =>  0,
+                'is_reserved'   =>  1
+            );
+            try {
+                civicrm_api3('ContactType', 'Create', $contactTypeParams);
+                $createdContactTypes[] = $contactType['name'];
+            } catch (CiviCRM_API3_Exception $e) {
+                CRM_Utils_System::setUFMessage("Could not create contact subtype {$contactType['name']}");
+            }    
+        }
+    }
+    $message = "Contact Subtypes ".implode(", ", $createdContactTypes)." succesfully created";
+    CRM_Utils_System::setUFMessage($message);
+    /*
+     * activityTypes
+     */
+    $createdActivityTypes = array();
+    try {
+        $existingActivityTypes = civicrm_api3('ActivityTypes', 'Get');
+    } catch (CiviCRM_API3_Exception $e) {
+        $existingActivityTypes = array();
+    }
+    foreach ($requiredActivityTypes as $activityTypeName) {
+        /*
+         * check if activityType exists
+         */
+        if (!in_array($activityTypeName, $existingActivityTypes)) {
+            $activityTypeParams = array(
+                'name'      =>  $activityTypeName,
+                'label'     =>  $activityTypeName,
+                'weight'    =>  30,
+                'is_active' =>  0
+            );
+            try {
+               civicrm_api3('ActivityType', 'Create', $activityTypeParams);
+               $createdActivityTypes[] = $activityTypeName;
+            }
+            catch (CiviCRM_API3_Exception $e) {
+                CRM_Utils_System::setUFMessage("Could not generate activity type $activityTypeName");
+            }
+        }
+    }
+    if (!empty($createdActivityTypes)) {
+        $message = "Activity Types ".implode(", ", $createdActivityTypes)." successfully created";
+        CRM_Utils_System::setUFMessage($message);
+    }    
+    return _generic_civix_civicrm_install();
+}
+/**
+ * Implementation of hook_civicrm_enable
+ * 
+ * @author Erik Hommel (erik.hommel@civicoop.org, http://www.civicoop.org)
+ * @date 2 Dec 2013
+ * 
+ * Enable activity types and option groups
+ */
+function generic_civicrm_enable() {
+    $requiredActivityTypes = array("First Interview Expert");
+    $requiredContactTypes = array("Expert");
+    /*
+     * set all contact types to enabled
+     */
+    foreach ($requiredContactTypes as $contactTypeName) {
+        $enableContactTypes = 
+            "UPDATE civicrm_contact_type SET is_active = 1 WHERE name = '$contactTypeName'";
+        CRM_Core_DAO::executeQuery($enableContactTypes);
+    }
+   /*
+     * set all activity types to enabled
+     */
+    foreach ($requiredActivityTypes as $activityTypeName) {
+        $enableActivityTypes = 
+            "UPDATE civicrm_option_value SET is_active = 1 WHERE option_group_id = 2 
+                AND name = '$activityTypeName'";
+        CRM_Core_DAO::executeQuery($enableActivityTypes);
+    }
+    return _generic_civix_civicrm_enable();
+}
+/**
+ * Implementation of hook_civicrm_disable
+ * 
+ * @author Erik Hommel (erik.hommel@civicoop.org, http://www.civicoop.org)
+ * @date 2 Dec 2013
+ * 
+ * Disable activity types and option groups
+ */
+function generic_civicrm_disable() {
+    $requiredActivityTypes = array("First Interview Expert");
+    $requiredContactTypes = array("Expert");
+    /*
+     * set all contact types to disabled
+     */
+    foreach ($requiredContactTypes as $contactTypeName) {
+        $disableContactTypes = 
+            "UPDATE civicrm_contact_type SET is_active = 0 WHERE name = '$contactTypeName'";
+        CRM_Core_DAO::executeQuery($disableContactTypes);
+    }
+    /*
+     * set all activity types to disabled
+     */
+    foreach ($requiredActivityTypes as $activityTypeLabel) {
+        $disableActivityTypes = 
+            "UPDATE civicrm_option_value SET is_active = 0 WHERE option_group_id = 2 
+                AND label = '$activityTypeLabel'";
+        CRM_Core_DAO::executeQuery($disableActivityTypes);
+    }
+    return _generic_civix_civicrm_disable();
+}
+/**
  * Implementation of hook_civicrm_managed
  * 
  * @author Erik Hommel (erik.hommel@civicoop.org - http://www.civicoop.org)
+ * @date 24 Nov 2013
  *
  * Generate a list of entities to create/deactivate/delete when this module
  * is installed, disabled, uninstalled.
  */
 function generic_civicrm_managed(&$entities) {
     $relationshipTypesCreated = array();
-    $activityTypesCreated = array();
     $groupsCreated = array();
-    $contactSubTypesCreated = array();
     /*
      * create specific groups for PUM
      */
@@ -89,79 +227,7 @@ function generic_civicrm_managed(&$entities) {
     );
     $relationshipTypesCreated[] = "Project Officer";
     $message = "Relationship Types ".implode(", ", $relationshipTypesCreated)." successfully created";
-    CRM_Utils_System::setUFMessage($message);
-
-    /*
-     * create specific activity types for PUM
-     */
-    $entities[] = array(
-        'module'    => 'nl.pum.generic',
-        'name'      => 'First Interview',
-        'entity'    => 'ActivityType',
-        'params'    => array(
-            'version'       => 3,
-            'label'         => 'First Interview Expert',
-            'name'          => 'First Interview Expert',
-            'weight'        => 99,
-            'is_active'     => 1,
-            'description'   => 'Activity for first selection interview with candidate expert'
-        )
-    );
-    $activityTypesCreated[] = "First Interview Expert";
-    $message = "Activity Types ".implode(", ", $activityTypesCreated). " successfully created";
-    CRM_Utils_System::setUFMessage($message);
+    CRM_Utils_System::setUFMessage($message);    
     
-    /*
-     * create specific contact subtype for PUM
-     */
-    $entities[] = array(
-        'module'    => 'nl.pum.generic',
-        'name'      => 'Expert',
-        'entity'    => 'ContactType',
-        'params'    => array(
-            'version'       => 3,
-            'label'         => 'Expert',
-            'name'          => 'Expert',
-            'is_active'     => 1,
-            'description'   => 'Contact subtype for PUM Expert',
-            'parent_id'     =>  1
-        )
-    );
-    $contactSubTypesCreated[] = "Expert";
-    $message = "Contact Subtypes ".implode(", ", $contactSubTypesCreated). " successfully created";
-    CRM_Utils_System::setUFMessage($message);
-    
-    /*
-     * create specific option groups for Marital Status and Nationality
-     */
-    $optionGroupsCreated = array();
-    $entities[] = array(
-        'module'    =>  'nl.pum.generic',
-        'name'      =>  'Marital Status',
-        'entity'    =>  'OptionGroup',
-        'params'    =>  array(
-            'version'       =>  3,
-            'title'         =>  "Marital Status",
-            'name'          =>  "marital_status",
-            'is_reserved'   =>  1
-        )
-    );
-    $optionGroupsCreated[] = "Marital Status";
-
-    $optionGroupsCreated = array();
-    $entities[] = array(
-        'module'    =>  'nl.pum.generic',
-        'name'      =>  'Nationality',
-        'entity'    =>  'OptionGroup',
-        'params'    =>  array(
-            'version'       =>  3,
-            'title'         =>  "Nationality",
-            'name'          =>  "nationality",
-            'is_reserved'   =>  1
-        )
-    );
-    $optionGroupsCreated[] = "Nationality";
-    CRM_Utils_System::setUFMessage("Option groups ".implode(", ", $optionGroupsCreated)." successfully created");
-
     return _generic_civix_civicrm_managed($entities);    
 }
