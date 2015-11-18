ad5782f077ab41c9f4c73e1f454d9ba018f66372
ruudgithub <ruudgithub@users.noreply.github.com>
2015-11-18 15:52:39 +0100

    Added extra checks for table existance

--- CRM/Generic/Page/ImportPUMHistory.php
+++ CRM/Generic/Page/ImportPUMHistory.php
@@ -14,57 +14,73 @@ class CRM_Generic_Page_ImportPUMHistory extends CRM_Core_Page {
 	$grp_prinshistory = generic_getCustomTableInfo('prins_history');
 	$grp_shortname = generic_getCustomTableInfo('Additional_Data');
 	
-	//Fetch Shortname
-	$sql_shortname = "SELECT * FROM ".$grp_shortname['group_table'];
-	$dao_shortname = CRM_Core_DAO::executeQuery($sql_shortname);
+	if (isset($grp_prinshistory['group_table']) && !is_null($grp_prinshistory['group_table']) &&
+		isset($grp_shortname['group_table']) &&	!is_null($grp_shortname['group_table'])) {
 		
-	while ($dao_shortname->fetch()) {
-		$shortname = $dao_shortname->$grp_shortname['columns']['Shortname']['column_name'];
-		
-		//Set filename and import directory
-		$file = 'pum_experience_'.$shortname.'.txt';
-		$dir_import = dirname(__FILE__).'/../../../importfiles/';
-		$file_import_fullpath = $dir_import.$file;
-		
-		if (file_exists($file_import_fullpath)) {
-			try {
+		if ((CRM_Core_DAO::checkTableExists($grp_prinshistory['group_table']) == TRUE) &&
+			(CRM_Core_DAO::checkTableExists($grp_shortname['group_table']) == TRUE)) {
+			//Fetch Shortname
+			$sql_shortname = "SELECT * FROM ".$grp_shortname['group_table'];
+			$dao_shortname = CRM_Core_DAO::executeQuery($sql_shortname);
 				
-				//Read file contents
-				$file_contents = file_get_contents($file_import_fullpath);
+			while ($dao_shortname->fetch()) {
+				$shortname = $dao_shortname->$grp_shortname['columns']['Shortname']['column_name'];
 				
-				//Count number or projects
-				//File format is an empty line for each new project
-				$count_projects = 0;
-				$empty_lines = explode(Chr(10).Chr(13), $file_contents);
-				$count_projects = count($empty_lines) - 1; //-1 because last lines are 2 empty lines
-								
-				if (strpos($file_contents,Chr(10)) != FALSE) {
-					$file_contents = str_replace(Chr(10),'<br />',$file_contents);
-				} elseif (strpos($file_contents,Chr(13)) != FALSE) {
-					$file_contents = str_replace(Chr(13),'<br />',$file_contents);
-				}
-								
-				//Determine if prins_history data is already available, if so skip it
-				$sql_checkavailability = "SELECT * FROM ".$grp_prinshistory['group_table']." WHERE entity_id = '".$dao_shortname->entity_id."'";
-				$dao_checkavailability = CRM_Core_DAO::executeQuery($sql_checkavailability);
-				$dao_checkavailability->fetch();
-			
-				if ($dao_checkavailability->N > 0) {
-					//Do not import anything, data already available
-					CRM_Core_Error::debug_log_message($shortname.' skipped, data is already available.');
+				//Set filename and import directory
+				$file = 'pum_experience_'.$shortname.'.txt';
+				$dir_import = dirname(__FILE__).'/../../../importfiles/';
+				$file_import_fullpath = $dir_import.$file;
+				
+				if (file_exists($file_import_fullpath)) {
+					try {
+						
+						//Read file contents
+						$file_contents = file_get_contents($file_import_fullpath);
+						
+						//Count number or projects
+						//File format is an empty line for each new project
+						$count_projects = 0;
+						$empty_lines = explode(Chr(10).Chr(13), $file_contents);
+						$count_projects = count($empty_lines) - 1; //-1 because last lines are 2 empty lines
+										
+						if (strpos($file_contents,Chr(10)) != FALSE) {
+							$file_contents = str_replace(Chr(10),'<br />',$file_contents);
+						} elseif (strpos($file_contents,Chr(13)) != FALSE) {
+							$file_contents = str_replace(Chr(13),'<br />',$file_contents);
+						}
+										
+						//Determine if prins_history data is already available, if so skip it
+						$sql_checkavailability = "SELECT * FROM ".$grp_prinshistory['group_table']." WHERE entity_id = '".$dao_shortname->entity_id."'";
+						$dao_checkavailability = CRM_Core_DAO::executeQuery($sql_checkavailability);
+						$dao_checkavailability->fetch();
+					
+						if ($dao_checkavailability->N > 0) {
+							//Do not import anything, data already available
+							CRM_Core_Error::debug_log_message($shortname.' skipped, data is already available.');
+						} else {
+							//Insert data into prins_history field
+							$sql = "INSERT INTO ".$grp_prinshistory['group_table']." (entity_id, ".$grp_prinshistory['columns']['prins_history']['column_name'].",".$grp_prinshistory['columns']['prins_history_number_of_projects']['column_name'].") VALUES ('".$dao_shortname->entity_id."','".addslashes($file_contents)."','".$count_projects."')";
+							$dao = CRM_Core_DAO::executeQuery($sql);
+						}
+					} catch (Exception $e) {
+						CRM_Core_Error::debug_log_message($e);
+					}
 				} else {
-					//Insert data into prins_history field
-					$sql = "INSERT INTO ".$grp_prinshistory['group_table']." (entity_id, ".$grp_prinshistory['columns']['prins_history']['column_name'].",".$grp_prinshistory['columns']['prins_history_number_of_projects']['column_name'].") VALUES ('".$dao_shortname->entity_id."','".addslashes($file_contents)."','".$count_projects."')";
-					$dao = CRM_Core_DAO::executeQuery($sql);
+					//Skip, no data available for import
 				}
-			} catch (Exception $e) {
-				CRM_Core_Error::debug_log_message($e);
+				
+				$this->assign('result', 'Import Successful');
+				CRM_Core_Error::debug_log_message('Import Successful');
 			}
 		} else {
-			//Skip, no data available for import
-		}
-	}
-	
+			$this->assign('result', 'Table does not exist');
+			CRM_Core_Error::debug_log_message('Table does not exist');
+	 	}	
+	} else {
+		$this->assign('result', 'Could not retrieve table');
+		CRM_Core_Error::debug_log_message('Could not retrieve table');
+ 	}
+ 	
     parent::run();
   }
 }
--- templates/CRM/Generic/Page/ImportPUMHistory.tpl
+++ templates/CRM/Generic/Page/ImportPUMHistory.tpl
@@ -3,3 +3,5 @@
 This page should be used only once, although it will check for existing entries.
 <br />
 Please check your CiviCRM log file for any errors during import.
+<br /><br />
+Result: {$result}
